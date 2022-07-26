f94744c9f21ea0466f0863476fde2b7cd71e7d84
onnyx1 <spencer.retcher1@gmail.com>
2022-07-25 20:16:56 -0400

    Completed Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+
+    if(num < 0){
+        return "ERROR";
+    }
+
+    let x = "";
+
+    for(let i = 0; i <num; i++){
+
+        x += string;
+    }
+
+    return x;
 
 };
 
