e07eae501cfa6db337c5add112e3e9891fc3dac8
onnyx1 <spencer.retcher1@gmail.com>
2022-07-25 22:03:03 -0400

    Completed Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
+const ftoc = function(num) {
+
+return Math.round( ((num -32) * 5/9) * 10)/10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  return Math.round( ((num * 1.8) + 32)*10)/10;
 };
 
 // Do not edit below this line
