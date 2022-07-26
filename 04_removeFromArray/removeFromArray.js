b8c44a28b595ca4e10773d41660b64c4861dfa3d
onnyx1 <spencer.retcher1@gmail.com>
2022-07-25 21:00:06 -0400

    Completed Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
 
-};
+ let list = array.filter(
+
+    function(value) {
+
+        if(items.indexOf(value) != -1){
+            items.splice(items.indexOf(value),1);
+            return false;
+        }
+
+        return true;
+
+    }
+ )
+
+ return list;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
