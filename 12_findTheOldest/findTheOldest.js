845082b5d2cff824652f4109bb19e44daa3a43f4
onnyx1 <spencer.retcher1@gmail.com>
2022-08-04 23:00:59 -0400

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(ar) {
+
+
+    let x = ar.reduce((obj, person) => {
+
+        if(person.yearOfDeath === undefined){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+
+
+        let age = person.yearOfDeath - person.yearOfBirth;
+
+        if(age > obj.age){
+            obj.age = age;
+            obj.name = person.name;
+        }
+
+        return obj;
+    }
+
+,{age:0})
+
+return x;
 
 };
 
