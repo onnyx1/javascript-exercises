4a4fdf2f0086df45448a5380d3201aac9cb7a78c
onnyx1 <spencer.retcher1@gmail.com>
2022-08-04 22:46:05 -0400

    Completed Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let n1 = 0, n2 = 1, nextTerm;
+
+    if( n < 0){
+        return "OOPS";
+    }
+    
+    for (let i = 1; i <= n; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+
+
+
+    }
+
+    return n1;
+
 
 };
 
