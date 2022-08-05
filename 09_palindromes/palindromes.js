4d6e1a528a1fc0641cc07bcfbf423aee4a34f569
onnyx1 <spencer.retcher1@gmail.com>
2022-08-04 22:27:55 -0400

    Completed Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+const newWord = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ? " "]/g,"").toLowerCase();
+console.log(newWord);
+console.log(Array.from(newWord).reverse().join(""));
+return Array.from(newWord).reverse().join("") === newWord;
 };
 
 // Do not edit below this line
